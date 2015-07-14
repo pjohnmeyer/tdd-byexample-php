9764da500f85ae63284b576b1b696d7b980e08ba
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2015-07-13 23:48:50 -0500

    Green: Page 28 (Dollar extends Money)

--- Dollar.php
+++ Dollar.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once 'Money.php';
+
 /**
  * Created by PhpStorm.
  * User: patrick.johnmeyer
  * Date: 7/6/15
  * Time: 10:32 PM
  */
-class Dollar
+class Dollar extends Money
 {
     private $amount;
 
