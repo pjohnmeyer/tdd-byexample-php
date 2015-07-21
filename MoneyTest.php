03fd3b6a99880bc72238fc0c7f6f9e230c1e2af6
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2015-07-21 00:32:24 -0500

    Green: Combine testMultiplication and testFrancMultiplication

--- MoneyTest.php
+++ MoneyTest.php
@@ -13,9 +13,11 @@ class TestMoney extends PHPUnit_Framework_TestCase
 {
     public function testMultiplication()
     {
-        $five = new Dollar(5);
-        $this->assertEquals(new Dollar(10), $five->times(2));
-        $this->assertEquals(new Dollar(15), $five->times(3));
+        $fiveDollars = new Dollar(5);
+        $this->assertEquals(new Dollar(10), $fiveDollars->times(2));
+
+        $fiveFrancs = new Franc(5);
+        $this->assertEquals(new Franc(15), $fiveFrancs->times(3));
     }
 
     // PHP thankfully gives us member-wise equality for free
@@ -26,12 +28,5 @@ class TestMoney extends PHPUnit_Framework_TestCase
         $this->assertEquals(new Franc(5), new Franc(5));
         $this->assertNotEquals(new Franc(5), new Franc(6));
         $this->assertNotEquals(new Franc(5), new Dollar(5));
-    }
-
-    public function testFrancMultiplication()
-    {
-        $five = new Franc(5);
-        $this->assertEquals(new Franc(10), $five->times(2));
-        $this->assertEquals(new Franc(15), $five->times(3));
     }
 }
