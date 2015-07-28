f0f3ca4616df72cf6eb52ae82d92713c3a89ee30
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2015-07-27 22:33:46 -0500

    Red: Page 63 (Add testReduceSum)

--- MoneyTest.php
+++ MoneyTest.php
@@ -54,4 +54,12 @@ class TestMoney extends PHPUnit_Framework_TestCase
         $this->assertEquals($five, $sum->augend); // (FWIW I normally use "lhs" and "rhs")
         $this->assertEquals($five, $sum->addend);
     }
+
+    public function testReduceSum()
+    {
+        $sum = new Sum(Money::dollar(3), Money::dollar(4));
+        $bank = new Bank();
+        $result = $bank->reduce($sum, 'USD');
+        $this->assertEquals(Money::dollar(7), $result);
+    }
 }
